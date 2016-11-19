8579d150ee3e69e9e1f746f634227c300b0f4c96
Craig S. Cottingham <craig@cottingham.net>
2016-11-18 21:03:07 -0600

    Add leds.py

---
+++ leds.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import string
+import time
+
+pindefs = {
+    'A':  4, 'B':  2, 'C': 27, 'D': 22, 'E': 26, 'F': 19, 'G': 13, 'H':  6, 'I':  5, 'J': 11, 'K':  9, 'L': 10, 'M':  3,
+    'N': 14, 'O': 15, 'P':  8, 'Q': 18, 'R': 23, 'S': 21, 'T': 17, 'U': 24, 'V': 25, 'W':  7, 'X': 12, 'Y': 16, 'Z': 20,
+}
+
+def blink(pin, delay_during=0.5, delay_after=0.5):
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(delay_during)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(delay_after)
+
+GPIO.setmode(GPIO.BCM)
+
+for pin in pindefs.values():
+    GPIO.setup(pin, GPIO.OUT)
+
+for letter in list(string.ascii_uppercase):
+    blink(pindefs[letter], 0.3, 0)
+
+time.sleep(2)
+
+blink(pindefs["H"])
+blink(pindefs["E"])
+blink(pindefs["L"])
+blink(pindefs["L"])
+blink(pindefs["O"])
+
+GPIO.cleanup()
