934858d70a60f75b971ccfc86c7b2fbb63d724a8
Craig S. Cottingham <craig@cottingham.net>
2017-01-04 23:26:10 -0600

    Add UpsideDownLeds.Supervisor

---
+++ lib/upside_down_leds/supervisor.ex
@@ -0,0 +1,25 @@
+defmodule UpsideDownLeds.Supervisor do
+  use Supervisor
+
+  ##
+  ## API
+  ##
+
+  @doc """
+  Starts the supervisor.
+  """
+  def start_link do
+    Supervisor.start_link(__MODULE__, :ok)
+  end
+
+  ##
+  ## callbacks
+  ##
+
+  def init(:ok) do
+    children = [
+      worker(UpsideDownLeds.Server, self)
+    ]
+    supervise(children, strategy: :one_for_all)
+  end
+end
