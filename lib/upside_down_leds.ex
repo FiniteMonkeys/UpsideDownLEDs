fbacb9b75b5b18f92c4169a8433c23c5c5296e79
Craig S. Cottingham <craig@cottingham.net>
2018-04-04 22:49:48 -0500

    Generate nerves project

--- lib/upside_down_leds.ex
+++ lib/upside_down_leds.ex
@@ -1,20 +1,20 @@
 defmodule UpsideDownLeds do
-  use GenServer
-
-  ## client API
-
-  @doc """
-  Starts the server.
-  """
-  def start_link(options \\ {}) do
-    GenServer.start_link(__MODULE__, options, [])
-  end
-
-  ## server callbacks
-
-  def init(_options \\ {}) do
-    {:ok, lights} = UpsideDownLeds.BlinkingLights.start_link
-    {:ok, twitter} = UpsideDownLeds.TwitterListener.start_link({ fn text -> UpsideDownLeds.BlinkingLights.puts(lights, text) end })
-    {:ok, lights: lights, twitter: twitter}
-  end
+  # use GenServer
+  #
+  # ## client API
+  #
+  # @doc """
+  # Starts the server.
+  # """
+  # def start_link(options \\ {}) do
+  #   GenServer.start_link(__MODULE__, options, [])
+  # end
+  #
+  # ## server callbacks
+  #
+  # def init(_options \\ {}) do
+  #   {:ok, lights} = UpsideDownLeds.BlinkingLights.start_link
+  #   {:ok, twitter} = UpsideDownLeds.TwitterListener.start_link({ fn text -> UpsideDownLeds.BlinkingLights.puts(lights, text) end })
+  #   {:ok, lights: lights, twitter: twitter}
+  # end
 end
